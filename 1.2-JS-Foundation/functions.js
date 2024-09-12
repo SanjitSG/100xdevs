0e1ed779a9f72ee130a836018f9b41423d9b9772
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-09-12 10:53:23 +0530

    Terminal clock

--- 1.2-JS-Foundation/functions.js
+++ 1.2-JS-Foundation/functions.js
@@ -1,3 +1,6 @@
+/**
+ * 
+ 
 let time = 10;
 
 let timerId = setInterval(countDown, 1000);
@@ -8,6 +11,40 @@ function countDown() {
     console.log("time's up");
   } else {
     console.log(time);
-    time--;
-  }
+  time--;
 }
+}
+*/
+
+// let startTime = Date.now();
+
+// setTimeout(() => {
+//   let endTime = Date.now();
+
+//   let timeTaken = endTime - startTime;
+
+//   console.log(`Time taken: ${timeTaken} ms`);
+// }, 1000);
+
+// let startTime = performance.now();
+
+// setTimeout(function () {
+//   let endTime = performance.now();
+//   let timeTaken = endTime - startTime;
+//   console.log(`Time taken: ${timeTaken.toFixed(2)} ms`);
+// }, 1000);
+
+function displayClock() {
+  let now = new Date();
+
+  let hours = String(now.getHours()).padStart(2, "0");
+  let minutes = String(now.getMinutes()).padStart(2, "0");
+  let seconds = String(now.getSeconds()).padStart(2, "0");
+
+  let timeString = `${hours}:${minutes}:${seconds}`;
+
+  console.clear();
+  console.log(timeString);
+}
+
+setInterval(displayClock, 1000);
