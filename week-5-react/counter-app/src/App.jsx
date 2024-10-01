455e50c1524cc1faefe0da2c8384ed92eff49e09
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-10-01 12:10:38 +0530

    simple todo

--- week-5-react/counter-app/src/App.jsx
+++ week-5-react/counter-app/src/App.jsx
@@ -1,14 +1,41 @@
 import { useState } from "react";
 import "./App.css";
-
+// todo application
+/**
+ *
+ * todo : [{title: "todo1", description: "desc", completed: false}]
+ *
+ */
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([
+    { title: "Study DSA", description: "Study Striver DSA", completed: false },
+    { title: "Study Dev", description: "Study harkarit dev", completed: false },
+  ]);
 
   return (
     <>
-      <button onClick={() => setCount(count + 1)}>Counter {count}</button>
+      {todos.map((todo, index) => {
+        return (
+          <Todo
+            title={todo.title}
+            description={todo.description}
+            key={index}
+          />
+        );
+      })}
     </>
   );
+
+  function Todo(props) {
+    const { title, description, completed, index } = props;
+
+    return (
+      <div key={index}>
+        <h1>{title}</h1>
+        <h2>{description}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
