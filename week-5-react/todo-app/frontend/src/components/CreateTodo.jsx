b54fccd76e867929ee26a011050ad1222bc0e9e0
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-10-03 01:21:12 +0530

    simple todo app

--- week-5-react/todo-app/frontend/src/components/CreateTodo.jsx
+++ week-5-react/todo-app/frontend/src/components/CreateTodo.jsx
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateTodo = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <>
-      <input type="text" name="title" id="title" placeholder="Title" style={{ padding: 10, margin: 10 }} /> <br />
-      <input type="text" name="title" id="title" placeholder="Description" style={{ padding: 10, margin: 10 }} /> <br />
-      <button>Add a Todo</button>
+      <input
+        type="text"
+        name="title"
+        id="title"
+        placeholder="Title"
+        style={{ padding: 10, margin: 10 }}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />{" "}
+      <br />
+      <input
+        type="text"
+        name="title"
+        id="title"
+        placeholder="Description"
+        style={{ padding: 10, margin: 10 }}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />{" "}
+      <br />
+      <button
+        onClick={() => {
+          fetch("http://localhost:3000/todo", {
+            method: "POST",
+            body: JSON.stringify({
+              title: title,
+              description: description,
+            }),
+            headers: {
+              "Content-type": "application/json",
+            },
+          }).then(async function (res) {
+            const json = await res.json();
+            alert("Todo Added");
+          });
+        }}>
+        Add a Todo
+      </button>
     </>
   );
 };
--- week-5-react/todo-app/frontend/src/components/Todos.jsx
+++ week-5-react/todo-app/frontend/src/components/Todos.jsx
@@ -2,9 +2,9 @@ const Todos = ({ todos }) => {
   if (!todos) return;
   return (
     <div>
-      {todos.map((todo) => {
+      {todos.map((todo, index) => {
         return (
-          <div>
+          <div key={index}>
             <h1>{todo?.title}</h1>
             <h2>{todo?.description}</h2>
             <button>{todo.completed == true ? "Completed" : "Mark as complete"}</button>
