c94d871657260518bfe9babc987ee7e59a8a115e
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-10-15 23:41:25 +0530

    sidebar

--- week-8/tailwind-pract/src/App.jsx
+++ week-8/tailwind-pract/src/App.jsx
@@ -1,15 +1,19 @@
 import "./App.css";
 import RevenueCard from "./components/RevenueCard";
+import { SidebarComponent } from "./components/Sidebar";
 
 function App() {
 	return (
-		<div className="grid grid-cols-3">
-			<RevenueCard
-				title={"Amount Pending"}
-				amount={"92,312.20"}
-				orderCount={13}
-			/>
-		</div>
+		<>
+			<SidebarComponent />
+			<div className="grid grid-cols-3">
+				<RevenueCard
+					title={"Amount Pending"}
+					amount={"92,312.20"}
+					orderCount={13}
+				/>
+			</div>
+		</>
 	);
 }
 
--- week-8/tailwind-pract/tailwind.config.js
+++ week-8/tailwind-pract/tailwind.config.js
@@ -1,18 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: [flowbite.content(), "./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
         blue: {
-          700: "#146eb4"
-        }
-      }
+          700: "#146eb4",
+        },
+      },
     },
   },
-  plugins: [],
-}
-
+  plugins: [flowbite.plugin()],
+};
