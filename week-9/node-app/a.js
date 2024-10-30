2f12884e1a26c4cfd9e342b0245965ae627ac9d5
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-10-31 01:37:12 +0530

    typescript basics

--- week-9/node-app/a.js
+++ week-9/node-app/a.js
@@ -1,3 +1,24 @@
 "use strict";
-const x = 1;
-console.log(x);
+// 1. Giving type to function argument
+function greet(firstName) {
+    console.log(`Hello ${firstName}`);
+}
+greet("Sanjit");
+// return type of a function (explicite)
+function sum(a, b) {
+    return a + b;
+}
+const x = sum(10, 2);
+// implicite return type - based on internal logic
+function isLegal(age) {
+    if (age > 18)
+        return true;
+    return false;
+}
+const st = isLegal(20);
+function pa(cb) {
+    setTimeout(cb, 1000);
+}
+pa(() => {
+    console.log("Hello");
+});
--- week-9/node-app/a.ts
+++ week-9/node-app/a.ts
@@ -1,2 +1,31 @@
-const x: number = 1;
-console.log(x);
+// 1. Giving type to function argument
+function greet(firstName: string) {
+	console.log(`Hello ${firstName}`);
+}
+
+greet("Sanjit");
+
+// return type of a function (explicite)
+
+function sum(a: number, b: number): number {
+	return a + b;
+}
+
+const x = sum(10, 2);
+
+// implicite return type - based on internal logic
+function isLegal(age: number) {
+	if (age > 18) return true;
+	return false;
+}
+
+const st = isLegal(20); // st is of type boolean
+
+// giving type to a function as (callback) passed as parameter.
+function pa(cb: () => void): void {
+	setTimeout(cb, 1000);
+}
+
+pa(() => {
+	console.log("Hello");
+});
